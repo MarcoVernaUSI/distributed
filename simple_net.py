d9799e5d2a4abf6f00c730585e2771fb5aeb5464
MarcoVernaUSI <tbs.verna@gmail.com>
2019-05-02 18:14:12 +0200

    Network_Implemented
    Implemented the simple neural network

---
+++ simple_net.py
@@ -0,0 +1,140 @@
+# Trying to define the simplest possible neural net where the output layer of the neural net is a single
+# neuron with a "continuous" (a.k.a floating point) output.  I want the neural net to output a continuous
+# value based off one or more continuous inputs.  My real problem is more complex, but this is the simplest
+# representation of it for explaining my issue.  Even though I've oversimplified this to look like a simple
+# linear regression problem (y=m*x), I want to apply this to more complex neural nets.  But if I can't get
+# it working with this simple problem, then I won't get it working for anything more complex.
+
+import tensorflow as tf
+import random
+import numpy as np
+
+class Net:
+    def __init__(self, INPUT_DIMENSION, OUTPUT_DIMENSION, TRAINING_RUNS, BATCH_SIZE, VERF_SIZE):
+        self.INPUT_DIMENSION = INPUT_DIMENSION
+        self.OUTPUT_DIMENSION = OUTPUT_DIMENSION
+        self.TRAINING_RUNS = TRAINING_RUNS
+        self.BATCH_SIZE = BATCH_SIZE
+        self.VERF_SIZE = VERF_SIZE
+        self.batch_inputs = np.array([])
+        self.batch_outputs = np.array([])
+
+        # Create a placeholder for our input variable
+        self.x = tf.placeholder(tf.float32, [None, self.INPUT_DIMENSION])
+
+        # Create variables for our neural net weights and bias
+        self.W = tf.Variable(tf.zeros([self.INPUT_DIMENSION, self.OUTPUT_DIMENSION]))
+        self.b = tf.Variable(tf.zeros([self.OUTPUT_DIMENSION]))
+
+        # Define the neural net.  Note that since I'm not trying to classify digits as in the tensorflow mnist
+        # tutorial, I have removed the softmax op.  My expectation is that 'net' will return a floating point
+        # value.
+        self.net = tf.matmul(self.x, self.W) + self.b
+
+        # Create a placeholder for the expected result during TRAINING_RUNS 
+        self.expected = tf.placeholder(tf.float32, [None, self.OUTPUT_DIMENSION])
+
+        # Same training as used in mnist example
+        self.loss = tf.reduce_mean(tf.square(self.expected - self.net))
+        # cross_entropy = -tf.reduce_sum(expected*tf.log(tf.clip_by_value(net,1e-10,1.0)))
+        self.train_step = tf.train.GradientDescentOptimizer(0.12).minimize(self.loss)
+
+        self.sess = tf.Session()
+
+        self.init = tf.initialize_all_variables()
+        self.sess.run(self.init)
+
+#INPUT_DIMENSION = 10
+#OUTPUT_DIMENSION = 5
+#TRAINING_RUNS = 100
+#BATCH_SIZE = 10000
+#VERF_SIZE = 1
+
+    # Generate a bunch of data points and then package them up in the array format needed by
+    # tensorflow
+    def set_batch_data(self, t_data, targets):
+        self.batch_inputs = t_data
+        self.batch_outputs = targets
+
+    def train(self):
+        # Perform our training runs
+
+        for i in range(self.TRAINING_RUNS):
+            print("trainin run: ", i, )
+
+            #batch_inputs, batch_outputs = generate_batch_data(self.BATCH_SIZE)
+            batch_inputs= self.batch_inputs
+            batch_outputs = self.batch_outputs
+
+
+            # I've found that my weights and bias values are always zero after training, and I'm not sure why.
+            self.sess.run(self.train_step, feed_dict={self.x: batch_inputs, self.expected: batch_outputs})
+
+            # Test our accuracy as we train...  I am defining my accuracy as the error between what I
+            # expected and the actual output of the neural net.
+            # accuracy = tf.reduce_mean(tf.sub( expected, net))
+            self.accuracy = tf.subtract(self.expected, self.net)  # using just subtract since I made my verification size 1 for debug
+            # tf.subtract()
+            # Uncomment this to debug
+            # import pdb; pdb.set_trace()
+            
+            #print("W=%f, b=%f" % (sess.run(W), sess.run(b)))
+
+
+
+            #batch_inputs, batch_outputs = generate_batch_data(self.VERF_SIZE)   ################
+            
+
+            batch_inputs=self.batch_inputs[10:11]
+            batch_outputs = self.batch_outputs[10:11]
+
+
+
+            actual = self.sess.run(self.net, feed_dict={self.x: batch_inputs})
+            result = self.sess.run(self.accuracy, feed_dict={self.x: batch_inputs, self.expected: batch_outputs})
+
+            print("    progress: ")
+            print("      inputs: ", batch_inputs)
+            print("      outputs:", batch_outputs)
+            print("      actual: ", actual) 
+            print("      error: ", result)
+
+    def predict(self, batch_inputs):
+ 
+        actual = self.sess.run(self.net, feed_dict={self.x: batch_inputs})
+        
+        return(actual)
+
+
+
+
+# Generate two arrays, the first array being the inputs that need trained on, and the second array containing outputs.
+def generate_test_point():
+    x = random.uniform(-8, 8)
+
+    # To keep it simple, output is just -x.
+    out = -x
+
+    return (np.array([x]), np.array([out]))
+
+
+# Generate a bunch of data points and then package them up in the array format needed by
+# tensorflow
+def generate_batch_data(num):
+    xs = []
+    ys = []
+
+    for i in range(num):
+        x, y = generate_test_point()
+
+        xs.append(x)
+        ys.append(y)
+
+    return (np.array(xs), np.array(ys))
+
+
+#if __name__ == '__main__':
+#    N = Net(1,1,100,10000,1)
+#    batch_inputs, batch_outputs = generate_batch_data(10000)
+#    N.set_batch_data(batch_inputs,batch_outputs)
+#    N.train()
--- task1.py
+++ task1.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random as rand
+import tensorflow as tf
+from simple_net import Net
 
 def mktr(x, y):
 	return np.array([[1,0,x],
@@ -16,8 +18,8 @@ def mkrot(theta):	# non serve
 		[0,0,1]])
 
 def atr(v,dt):
-    return mktr(v*dt,0)  # note we translate along x
-    
+	return mktr(v*dt,0)  # note we translate along x
+	
 
 class Agent:
 	def __init__(self, initial_pose, number):
@@ -57,63 +59,110 @@ class Agent:
 
 class PID:
 
-    def __init__(self, Kp, Ki, Kd):
-    	self.Kp = Kp
-    	self.Ki = Ki
-    	self.Kd = Kd
-    	self.last_e = None
-    	self.sum_e = 0
+	def __init__(self, Kp, Ki, Kd):
+		self.Kp = Kp
+		self.Ki = Ki
+		self.Kd = Kd
+		self.last_e = None
+		self.sum_e = 0
 
-    def step(self, e, dt):
-    	""" dt should be the time interval from the last method call """
-    	if(self.last_e is not None):
-    		derivative = (e - self.last_e) / dt
-    	else:
-    		derivative = 0
-    	self.last_e = e
-    	self.sum_e += e * dt
-    	return self.Kp * e + self.Kd * derivative + self.Ki * self.sum_e
+	def step(self, e, dt):
+		""" dt should be the time interval from the last method call """
+		if(self.last_e is not None):
+			derivative = (e - self.last_e) / dt
+		else:
+			derivative = 0
+		self.last_e = e
+		self.sum_e += e * dt
+		return self.Kp * e + self.Kd * derivative + self.Ki * self.sum_e
 
-def run_simulation(timesteps, n_agents, L):
-    	states = np.zeros((timesteps,n_agents,5))
-    	target_vels = np.zeros((timesteps, n_agents))
-    	agents_list = [None] * n_agents
+def run_simulation(timesteps, n_agents, L, init= None):
+		states = np.zeros((timesteps,n_agents,5))
+		target_vels = np.zeros((timesteps, n_agents))
+		agents_list = [None] * n_agents
 
-    	### Initialize agents at random
-    	for i in range(n_agents):
-    		initial_pose = np.matmul(np.eye(3), mktr(rand.random()*L, 0))
-    		agents_list[i]=Agent(initial_pose,i)
-    	for i in range(n_agents):
-    		agents_list[i].state = agents_list[i].observe(agents_list,L)
-    	# Gli ordino in base alla posizione
-    	agents_list.sort(key=lambda x: x.getxy()[0], reverse=False)
+		### Initialize agents at random if not given initial state
+		if init is None:
+			for i in range(n_agents):
+				initial_pose = np.matmul(np.eye(3), mktr(rand.random()*L, 0))
+				agents_list[i]=Agent(initial_pose,i)
+		else:
+			for i in range(n_agents):
+				agents_list[i]=Agent(init[i],i)
 
-    	#save initial state
-    	states[0]= save_state(agents_list)
+		for i in range(n_agents):
+			agents_list[i].state = agents_list[i].observe(agents_list,L)
+		# Gli ordino in base alla posizione
+		agents_list.sort(key=lambda x: x.getxy()[0], reverse=False)
 
-    	# Controller onniscente
-    	goal_list = [None]*n_agents
-    	dist = L/(n_agents+1)
-    	for i in range(n_agents):
-    		goal_list[i]=dist*(i+1)
+		#save initial state
+		states[0]= save_state(agents_list)
 
-    	controller = PID(-0.8,0,0)
+		# Controller onniscente
+		goal_list = [None]*n_agents
+		dist = L/(n_agents+1)
+		for i in range(n_agents):
+			goal_list[i]=dist*(i+1)
 
-    	#Parameters
-    	mas_vel = 1 # m/s
-    	dt= 0.1
-    	for t in range (1, timesteps):
-    		for i in range( 0, len(agents_list)):
-    			error = agents_list[i].getxy()[0]-goal_list[i]
-    			v = controller.step(error, dt)
-    			v = np.clip(v, -mas_vel, +mas_vel)
-    			agents_list[i].step(v,dt)
-    			target_vels[t,i] = v
-    		for i in range( 0, len(agents_list)):
-    			agents_list[i].state = agents_list[i].observe(agents_list,L)
-    		states[t] = save_state(agents_list)
+		controller = PID(-0.8,0,0)
 
-    	return states, target_vels
+		#Parameters
+		mas_vel = 1 # m/s
+		dt= 0.1
+		for t in range (1, timesteps):
+			for i in range( 0, len(agents_list)):
+				error = agents_list[i].getxy()[0]-goal_list[i]
+				v = controller.step(error, dt)
+				v = np.clip(v, -mas_vel, +mas_vel)
+				agents_list[i].step(v,dt)
+				target_vels[t,i] = v
+			for i in range( 0, len(agents_list)):
+				agents_list[i].state = agents_list[i].observe(agents_list,L)
+			states[t] = save_state(agents_list)
+
+		return states, target_vels
+
+
+def run_simulation_learned(timesteps, n_agents, L, Net, init=None):
+		states = np.zeros((timesteps,n_agents,5))
+		target_vels = np.zeros((timesteps, n_agents))
+		agents_list = [None] * n_agents
+
+		### Initialize agents at random if not given initial state
+		if init is None:
+			for i in range(n_agents):
+				initial_pose = np.matmul(np.eye(3), mktr(rand.random()*L, 0))
+				agents_list[i]=Agent(initial_pose,i)
+		else:
+			for i in range(n_agents):
+				agents_list[i]=Agent(init[i],i)
+
+		# Gli ordino in base alla posizione
+		agents_list.sort(key=lambda x: x.getxy()[0], reverse=False)
+
+		#save initial state
+		states[0]= save_state(agents_list)
+
+		#Parameters
+		mas_vel = 1 # m/s
+		dt= 0.1
+		for t in range (1, timesteps):
+			inputs = states[t-1,:,3:]
+			inputs = inputs.reshape(1, -1)
+			predicted_velocities = Net.predict(inputs)
+			
+			for i in range( 0, len(agents_list)):
+#				error = agents_list[i].getxy()[0]-goal_list[i]
+				v = predicted_velocities[0,i]
+				v = np.clip(v, -mas_vel, +mas_vel)
+				agents_list[i].step(v,dt)
+			for i in range( 0, len(agents_list)):
+				agents_list[i].state = agents_list[i].observe(agents_list,L)
+			states[t] = save_state(agents_list)
+
+		return states
+
+
 
 
 
@@ -137,7 +186,7 @@ def plot_simulation(data, L):
 	y = data[0,:,1]
 
 	x_data = data[1:,:,0]
-    
+	
 	y_data = data[1:,:,1]
 
 	fig = plt.figure()
@@ -158,16 +207,63 @@ def plot_simulation(data, L):
 #	points = plt.plot([1,2,3,4,5], [0,0,0,0,0], 'ro')
 
 
-	for i in xrange(numframes):
+	for i in range(numframes):
 		ani = animation.FuncAnimation(fig, update_plot ,frames = numframes ,fargs=(x_data,y_data, scat))
 
 
+	plt.show()
+
+def plot_simulation2(data1,data2, L):
+
+	numframes = len(data1)-1
+	numpoints = len(data1[0])
+	x1 = data1[0,:,0]
+	y1 = data1[0,:,1]
+
+	x2 = data2[0,:,0]
+	y2 = data2[0,:,1]
+
+
+
+	x_data1 = data1[1:,:,0]	
+	y_data1 = data1[1:,:,1]
+
+	x_data2 = data2[1:,:,0]	
+	y_data2 = data2[1:,:,1]
+
+
+	fig = plt.figure()
+	
+
+	#data = np.random.rand(numframes, 2,5)
+	l, = plt.plot([], [], 'r-')
+	plt.xlim(0, L)
+	plt.ylim(-1, 1)
+	plt.xlabel('x')
+	plt.title('Task 1')
+
+
+	line = np.linspace(0, L)
+	plt.plot(line, 0*line);
+
+	scat1 = plt.scatter(x1, y1, s=100)
+	scat2 = plt.scatter(x2, y2, s=100, c='red')
+#	points = plt.plot([1,2,3,4,5], [0,0,0,0,0], 'ro')
+
+
+	for i in range(numframes):
+		ani = animation.FuncAnimation(fig, update_plot ,frames = numframes ,fargs=(x_data1,y_data1,x_data2,y_data2, scat1, scat2))
+
+	ani.save('plots/animation.gif', writer='imagemagick', fps=10)
 	plt.show()	
 
-def update_plot(i, x_data,y_data, scat):
-	data = np.array([x_data[i],y_data[i]]).T
-	scat.set_offsets(data)
-	return scat,
+def update_plot(i, x_data1,y_data1, x_data2,y_data2, scat1, scat2):
+	data1 = np.array([x_data1[i],y_data1[i]]).T
+	scat1.set_offsets(data1)
+	data2 = np.array([x_data2[i],y_data2[i]]).T
+	scat2.set_offsets(data2)
+	
+	return scat1, scat2
 		
 
 
@@ -180,9 +276,48 @@ def update_plot(i, x_data,y_data, scat):
 if __name__ == '__main__':
 	L = 10. # Distanza tra i due muri
 	N = 5 # Number of agents
-	timesteps = 100 #timesteps
+	timesteps = 120 #timesteps
+	n_simulation = 100 # number of simulations
 
-	states, target_vels = run_simulation(timesteps,N,L)
+	net_inputs = None
+	net_outputs = None
 
-	plot_simulation(states,L)
+
+	for i in range(n_simulation):
+		states, target_vels = run_simulation(timesteps,N,L)
+		
+		# prepara data for the training of the net
+		t_data=states[:,:,3:]
+		t_data = t_data.reshape(t_data.shape[0], -1)
+
+		if net_inputs is None:
+			net_inputs = t_data
+			net_outputs=target_vels
+		else:
+			net_inputs = np.append(net_inputs,t_data,axis=0)
+			net_outputs = np.append(net_outputs,target_vels,axis=0)
+
+
+	#train the net
+
+	Net = Net(net_inputs.shape[1],net_outputs.shape[1],1000,net_inputs.shape[0],1)
+	Net.set_batch_data(net_inputs,net_outputs)
+	Net.train()
+
+
+	# Make a confront of the two simulations
+	init = []
+	for i in range(N):
+		init.append(np.matmul(np.eye(3), mktr(rand.random()*L, 0)))
+
+	states, _ =run_simulation(timesteps,N,L,init)
+	states_L= run_simulation_learned(timesteps,N,L,Net,init)
+
+
+
+
+	#plot_simulation(states,L)
+	#plot_simulation(states_L,L)
+	plot_simulation2(states,states_L,L)
+	
 	print('ok')
